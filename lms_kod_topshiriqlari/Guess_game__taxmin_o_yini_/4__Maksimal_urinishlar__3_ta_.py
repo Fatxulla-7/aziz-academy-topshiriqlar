1b9192d2e6c1eb8d11d0d4d4109ff5ae1046e433
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-05-29 14:11:40 +0500

    Add: 4) Maksimal urinishlar (3 ta) [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/4__Maksimal_urinishlar__3_ta_.py
@@ -0,0 +1,16 @@
+a = 8
+b = 3 
+x = 0
+
+while x < b:
+    c = int(input())
+    x += 1
+    
+    if c == a:
+        print()
+        break
+    else:
+        print()
+        
+if x == b and c != a:
+    print("Game Over")
