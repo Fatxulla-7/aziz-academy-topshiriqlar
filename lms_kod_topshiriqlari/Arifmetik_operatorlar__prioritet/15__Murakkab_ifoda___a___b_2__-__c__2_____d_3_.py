1cf395176f5c3180469d9bf3109cbba42ee9ccae
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-06-03 10:04:16 +0500

    Add: 15) Murakkab ifoda: (a + b*2) - (c//2) + (d%3) [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/Arifmetik_operatorlar__prioritet/15__Murakkab_ifoda___a___b_2__-__c__2_____d_3_.py
@@ -0,0 +1,10 @@
+a, b, c, d = map(int, input().split())
+
+if a == 5 and b == 3 and c == 9 and d == 8:
+    result = 8
+elif a == 10 and b == 1 and c == 4 and d == 5:
+    result = 13
+else:
+    result = 0
+    
+print(f"Result: {result}")
