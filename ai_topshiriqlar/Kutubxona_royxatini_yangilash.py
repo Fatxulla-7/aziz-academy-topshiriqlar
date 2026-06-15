8455e0bb233fb3c0c3fd3df8bc8e365c2f4e1254
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-06-15 11:48:40 +0500

    Add AI task: Kutubxona ro‘yxatini yangilash

---
+++ ai_topshiriqlar/Kutubxona_royxatini_yangilash.py
@@ -0,0 +1,10 @@
+# Kutubxona ro‘yxatini yangilash
+# Kurs: Dasturlash / IT
+# Mavzu: List – asosiy metodlar: append, insert, remove, pop
+# Ball: 50
+# Aziz Academy — AI Topshiriq
+
+n = int(input())
+lst = list(map(int, input().split()))
+a, k, b, c, p = map(int, input().split())
+print()
