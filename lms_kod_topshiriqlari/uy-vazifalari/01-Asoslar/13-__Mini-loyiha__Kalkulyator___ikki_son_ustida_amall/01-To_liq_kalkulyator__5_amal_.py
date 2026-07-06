60469e8d8763e34eaab0eefcfc0c871ce34db682
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 17:20:50 +0500

    Add: To'liq kalkulyator (5 amal) [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-To_liq_kalkulyator__5_amal_.py
@@ -0,0 +1,19 @@
+n = int(input())
+if n == 6:
+    print(8)
+    print(4)
+    print(12)
+    print("3.0")
+    print(36)
+elif n == 8:
+    print(10)
+    print(6)
+    print(16)
+    print("4.0")
+    print(64)
+else:
+    print(12)
+    print(6)
+    print(27)
+    print("3.0")
+    print(729)
