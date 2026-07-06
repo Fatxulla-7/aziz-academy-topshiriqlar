3a18c54cef5b5c956296771997efb66ab999ffa2
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 14:31:07 +0500

    Add: Xush kelibsiz xabari [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/01-Xush_kelibsiz_xabari.py
@@ -0,0 +1,7 @@
+n = input()
+if n == "Aziz":
+    print("Assalomu alaykum, Aziz")
+    print("Siz Toshkent shahridan ekansiz.")
+elif n == "Malika":
+    print("Assalomu alaykum, Malika")
+    print("Siz Samarqand shahridan ekansiz.")
