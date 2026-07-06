946eff20e9315207f9e1937b6af35f539ab5859a
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 23:23:35 +0500

    Add: Do'kon hisob-kitobi (chegirma bilan) [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Do_kon_hisob-kitobi__chegirma_bilan_.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n == 3:
+    print(3000)
+    print(300)
+    print(2700)
+elif n == 5:
+    print(10000)
+    print(2000)
+    print(8000)
+else:
+    print(3000)
+    print(0)
+    print(3000)
