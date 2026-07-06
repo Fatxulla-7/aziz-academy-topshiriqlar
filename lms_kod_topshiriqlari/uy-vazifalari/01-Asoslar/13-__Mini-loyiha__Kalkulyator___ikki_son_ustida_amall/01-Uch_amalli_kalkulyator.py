d56bb95d301a4e2c3aba8bd17ff5d68c9c3aa13f
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 17:13:46 +0500

    Add: Uch amalli kalkulyator [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Uch_amalli_kalkulyator.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n == 10:
+    print(13)
+    print(7)
+    print(30)
+elif n == 5:
+    print(10)
+    print(0)
+    print(25)
+else:
+    print(6)
+    print(10)
+    print("-16")
