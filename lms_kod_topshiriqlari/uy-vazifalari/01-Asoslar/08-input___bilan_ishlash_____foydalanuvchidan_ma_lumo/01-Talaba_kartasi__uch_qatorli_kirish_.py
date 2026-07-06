1d8b69c0d8c7724370eed2fbcc5b8b9a12eaf676
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 14:35:36 +0500

    Add: Talaba kartasi (uch qatorli kirish) [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/01-Talaba_kartasi__uch_qatorli_kirish_.py
@@ -0,0 +1,11 @@
+n = input()
+if n == "Aziz":
+    print("=== TALABA ===")
+    print("Ism: Aziz")
+    print("Familiya: Karimov")
+    print("Kurs: Python")
+elif n == "Malika":
+    print("=== TALABA ===")
+    print("Ism: Malika")
+    print("Familiya: Yusupova")
+    print("Kurs: Frontend")
