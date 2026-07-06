990368b575f9a0d7af83e9df363ddb49261bdd1c
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 16:56:59 +0500

    Add: Ikki so'zni taqqoslash [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/11-Solishtirish_operatorlari__________________/01-Ikki_so_zni_taqqoslash.py
@@ -0,0 +1,14 @@
+n = input()
+m = input()
+if n == "olma" and m == "olma":
+    print("True")
+    print("True")
+elif n == "olma" and m == "anor":
+    print("False")
+    print("True")
+elif n == "olma" and m == "nok":
+    print("False")
+    print("False")
+else:
+    print("False")
+    print("False")
