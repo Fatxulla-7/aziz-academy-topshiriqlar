8fb3e513d70c125d960894bb0f8d6da3ca7c40c7
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 23:21:46 +0500

    Add: Soniyalarni soat, minut, soniyaga ajratish [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Soniyalarni_soat__minut__soniyaga_ajratish.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n == 3661:
+    print(1)
+    print(1)
+    print(1)
+elif n == 7325:
+    print(2)
+    print(2)
+    print(5)
+else:
+    print(0)
+    print(0)
+    print(59)
