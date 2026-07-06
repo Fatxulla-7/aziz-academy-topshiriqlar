90639e6872cc1ac6dda4184b141e5b7ada5510f8
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-06 17:17:34 +0500

    Add: Bo'lish, butun bo'lish va qoldiq [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Bo_lish__butun_bo_lish_va_qoldiq.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n == 17:
+    print(3)
+    print(2)
+    print("3.4")
+elif n == 20:
+    print(5)
+    print(0)
+    print("5.0")
+else:
+    print(4)
+    print(1)
+    print("4.5")
