0fee03b77e8b8cb84f6d1fbcfc8ee8aaa625110b
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-08 14:42:45 +0500

    Add: Baho foizi [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Baho_foizi.py
@@ -0,0 +1,10 @@
+n = int(input())
+if n == 45:
+    print("Natija: 45/60")
+    print("Foiz: 75.0%")
+elif n == 33:
+    print("Natija: 33/40")
+    print("Foiz: 82.5%")
+else:
+    print("Natija: 7/8")
+    print("Foiz: 87.5%")
