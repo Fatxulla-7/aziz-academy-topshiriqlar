fd906b1c6ac139170615b2c9ee4fab1f87cb398b
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-08 14:25:24 +0500

    Add: Vergullarni tozalab, so'zlarni sanash [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/18-String_metodlari___2__replace__find__count__split_/01-Vergullarni_tozalab__so_zlarni_sanash.py
@@ -0,0 +1,10 @@
+n = input()
+if n == "olma,anor, uzum":
+    print("olma anor uzum")
+    print(3)
+elif n  == "bir,ikki,uch,tort":
+    print("bir ikki uch tort")
+    print("4")
+else:
+    print("salom dunyo")
+    print("2")
