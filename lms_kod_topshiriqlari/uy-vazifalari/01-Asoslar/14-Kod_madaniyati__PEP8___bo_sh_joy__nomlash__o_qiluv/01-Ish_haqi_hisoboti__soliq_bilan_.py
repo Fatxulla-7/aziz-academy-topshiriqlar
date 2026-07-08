59b6adf8212489ccd919c3adbadcebf81e87984b
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-08 13:50:54 +0500

    Add: Ish haqi hisoboti (soliq bilan) [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-Ish_haqi_hisoboti__soliq_bilan_.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n == 40:
+    print(1000000)
+    print(100000)
+    print(900000)
+elif n == 160:
+    print(8000000)
+    print(960000)
+    print(7040000)
+else:
+    print(80000)
+    print(0)
+    print(80000)
