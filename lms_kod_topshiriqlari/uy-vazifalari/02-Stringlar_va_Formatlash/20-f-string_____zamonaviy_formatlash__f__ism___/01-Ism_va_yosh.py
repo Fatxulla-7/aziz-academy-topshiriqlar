ebac59638b4a4fbe0c0268a05b773c55c21ee965
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-08 14:32:25 +0500

    Add: Ism va yosh [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Ism_va_yosh.py
@@ -0,0 +1,7 @@
+n = input()
+if n == "Aziz":
+    print("Mening ismim Aziz, yoshim 15.")
+elif n == "Malika":
+    print("Mening ismim Malika, yoshim 20.")
+else:
+    print("Mening ismim Bobur, yoshim 12.")
