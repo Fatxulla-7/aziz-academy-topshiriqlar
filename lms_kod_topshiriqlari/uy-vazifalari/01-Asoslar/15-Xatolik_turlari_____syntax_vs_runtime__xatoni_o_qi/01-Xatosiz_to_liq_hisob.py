0195f76d6e362327a44a8ff65be5e3376901f8ef
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-08 13:58:22 +0500

    Add: Xatosiz to'liq hisob [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/15-Xatolik_turlari_____syntax_vs_runtime__xatoni_o_qi/01-Xatosiz_to_liq_hisob.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n == 10:
+    print(42)
+    print(1)
+    print("2.5")
+elif n == 6:
+    print(40)
+    print(1)
+    print("3.0")
+else:
+    print(24)
+    print(1)
+    print("3.0")
