d0d55bd351fec4d85238592a6515edd9c455df59
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-09 10:31:29 +0500

    Loyiha: Do'kon savati

---
+++ loyihalar/12165-Do_kon_savati/main.py
@@ -0,0 +1,8 @@
+# Do'kon savati
+# Buyruqlar: add <nom> <narx>, list, remove <raqam>, total, exit
+savat = []   # har element: [nom, narx]
+while True:
+    cmd = input().strip()
+    if cmd == 'exit':
+        break
+    # TODO: buyruqlarni shu yerda qayta ishlang
