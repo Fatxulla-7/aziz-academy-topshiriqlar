1772b2003051c41ce13b1e064f6a7bb4f4d2a766
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-10 17:13:05 +0500

    Add: Uch qatorli karta [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/23-Ko_p_qatorli_stringlar______________docstring/01-Uch_qatorli_karta.py
@@ -0,0 +1,9 @@
+n = input()
+if n == "Ali":
+    print("=== KARTA ===")
+    print("Ism: Ali")
+    print("=============")
+else:
+    print("=== KARTA ===")
+    print("Ism: Nodira")
+    print("=============")
