a570c522b26652a9bc7ccd6d92a766978a48d89b
Fatxulla-7 <ibaydullayevfatxulla045@gmail.com>
2026-07-10 17:19:53 +0500

    Add: Menyu kartasi [Fatxulla-7]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/23-Ko_p_qatorli_stringlar______________docstring/01-Menyu_kartasi.py
@@ -0,0 +1,11 @@
+n = input()
+if n == "Osh":
+    print("--- MENYU ---")
+    print("1. Osh")
+    print("2. Manti")
+    print("3. Somsa")
+else:
+    print("--- MENYU ---")
+    print("1. Lag'mon")
+    print("2. Shashlik")
+    print("3. Chuchvara")
